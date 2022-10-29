7d04c48ca7405225fb4b5cf68147dc4d5014cd04
max <maksim.kalganov@gmail.com>
2022-10-29 18:41:50 +0300

    add yandex_music DownloadInfo as input parameter for FeaturesGenerators

--- preprocessors/feature_extractors/abstract_feature_extractor.py
+++ preprocessors/feature_extractors/abstract_feature_extractor.py
@@ -1,7 +1,10 @@
 import abc
+import yandex_music as ym
+import numpy as np
 
 
 class FeatureExtractor(abc.ABC):
+
     @abc.abstractmethod
-    def get_feature(self):
+    def get_feature(self, download_info: ym.DownloadInfo, waveform: np.ndarray) -> float:
         pass

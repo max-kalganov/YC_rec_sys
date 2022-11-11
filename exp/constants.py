43f94477490a7af9797368d7e6d68ad9bf229f68
max <maksim.kalganov@gmail.com>
2022-11-11 15:39:11 +0300

    write constants

---
+++ exp/constants.py
@@ -0,0 +1,12 @@
+
+TRAIN_KEY = "train"
+TEST_KEY = "test"
+TRACKS_TO_ARTISTS_KEY = "track_artists"
+
+ALL_KEYS = {TRAIN_KEY, TEST_KEY, TRACKS_TO_ARTISTS_KEY}
+
+KEY_TO_FILENAME = {
+    TRAIN_KEY: "data/likes_data/train",
+    TEST_KEY: "data/likes_data/test",
+    TRACKS_TO_ARTISTS_KEY: "data/likes/track_artists.csv"
+}
